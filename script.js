6f18c69f066dd1a5233dc2a68666272d6ca2b09c
Alan Sobenes <alansobenes@gmail.com>
2021-09-24 17:05:38 -0700

    workingConvertDegrees

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ function removeCookieAlert(id){
     document.getElementById(id).remove();
 }
 
+
+
+var temps = document.getElementsByClassName('temp');
+
 function convertTemp(){
     console.log("here");
     if (document.getElementById('tempSelect').value == 'celcius'){
@@ -13,27 +17,14 @@ function convertTemp(){
 }
 function convertC(){
     console.log('back here');
-    var element = document.getElementById('temp');
-    var x = parseInt(element.innerHTML-32) * 5 / 9;
-    element.innerHTML= Math.round(x);
+    for(var i = 0; i < temps.length; i++){
+        temps[i].innerHTML= Math.round(parseInt(temps[i].innerHTML-32) * 5 / 9);
+    }
 }
 function convertF(){
-    console.log("now im Here")
-    var temps = document.getElementById('temp');
-    var x = parseInt(temps.innerHTML) * 9 / 5 + 32;
-    temps.innerHTML = Math.round(x);
+    console.log('here');
+    for(var i = 0; i < temps.length; i++){
+        temps[i].innerHTML= Math.round(parseInt(temps[i].innerHTML) * 9 / 5 + 32);
+    }
 }
-// function convertAllF(){
-//     var elements = document.querySelectorAll(".temp");
-//     for (var i = 0; i<elements.length; i++){
-//         elements[i].addEventListener("onchange", convertF())
 
-//     }
-// }
-// function convertallF(){
-//     var elements = document.querySelectorAll(".temp").forEach( item => {
-//         item.addEventListener("onchange", convertF())
-//     });
-//     array.forEach(element => {
-        
-//     }
